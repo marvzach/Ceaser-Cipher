8a740856435ec793b69c891ae99ee469d88e5fd3
Marvin Zachary <acerzach@gmail.com>
2019-04-08 12:32:50 +0300

    Added App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,33 +1,32 @@
-// import java.io.Console;
+import java.io.Console;
+public class App {
+    public static void main(String [] args){
+        Console myConsole = System.console();
+        System.out.println("Ceaser cypher. Please choose to either encrypt or decrypt by writing encrypt or vice verse");
+        String userChoice = myConsole.readLine();
+        if (userChoice.equalsIgnoreCase("encrypt")){
+            System.out.println("Enter text to encrypt");
+            String userEncryptionText = myConsole.readLine();
+            System.out.println("Please enter shift key");
+            String stringCypherKey = myConsole.readLine();
+            int intCypherKey = Integer.parseInt(stringCypherKey);
+            CeaserCypher ceaserCypher = new CeaserCypher();
+            String encryptedText = ceaserCypher.cypher(userEncryptionText, intCypherKey);
+            System.out.println(encryptedText);
+            System.out.println("-------------------------------------------");
+        } else if (userChoice.equalsIgnoreCase("decrypt")){
+            System.out.println("Enter text to decrypt");
+            String userDecryptionText = myConsole.readLine();
+            System.out.println("Please enter shift key");
+            String stringDecypherKey = myConsole.readLine();
+            int intDecypherKey = Integer.parseInt(stringDecypherKey);
+            CeaserCypher ceaserCypher = new CeaserCypher();
+            String decryptedText = ceaserCypher.decypher(userDecryptionText, intDecypherKey);
+            System.out.println(decryptedText);
+            System.out.println("-------------------------------------------");
+        } else {
+            System.out.println("Sorry, your input is invalid");
+        }
 
-// public class App {
-//     public static void main(String [] args){
-//         Console myConsole = System.console();
-//         System.out.println("Ceaser cypher. Please choose to either encrypt or decrypt by writing encrypt or vice verse");
-//         String userChoice = myConsole.readLine();
-//         if (userChoice.equalsIgnoreCase("encrypt")){
-//             System.out.println("Enter text to encrypt");
-//             String userEncryptionText = myConsole.readLine();
-//             System.out.println("Please enter shift key");
-//             String stringCypherKey = myConsole.readLine();
-//             int intCypherKey = Integer.parseInt(stringCypherKey);
-//             CeaserCypher ceaserCypher = new CeaserCypher();
-//             String encryptedText = ceaserCypher.cypher(userEncryptionText, intCypherKey);
-//             System.out.println(encryptedText);
-//             System.out.println("-------------------------------------------");
-//         } else if (userChoice.equalsIgnoreCase("decrypt")){
-//             System.out.println("Enter text to decrypt");
-//             String userDecryptionText = myConsole.readLine();
-//             System.out.println("Please enter shift key");
-//             String stringDecypherKey = myConsole.readLine();
-//             int intDecypherKey = Integer.parseInt(stringDecypherKey);
-//             CeaserCypher ceaserCypher = new CeaserCypher();
-//             String decryptedText = ceaserCypher.decypher(userDecryptionText, intDecypherKey);
-//             System.out.println(decryptedText);
-//             System.out.println("-------------------------------------------");
-//         } else {
-//             System.out.println("Sorry, your input is invalid");
-//         }
-
-//     }
-// }
+    }
+}
